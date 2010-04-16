3ba8136c3da65a905eda28fcad617f8d5ff9414e
Mark Doyle <markjohndoyle@googlemail.com>
2010-04-16 23:11:42 +0200

    Fixed some perspective id errors.  Set perspective default ordering in ini file.  Added provider data in manifests.

--- HummingbirdMcsCore/src/com/logica/hummingbird/hummingbirdmcscore/ApplicationWorkbenchAdvisor.java
+++ HummingbirdMcsCore/src/com/logica/hummingbird/hummingbirdmcscore/ApplicationWorkbenchAdvisor.java
@@ -6,7 +6,7 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	private static final String DEFAULT_PERSPECTIVE_ID = "com.logica.hummingbird.hummingbirdmcscore.perspectives.FrameBrokerPerspective";
+	private static final String DEFAULT_PERSPECTIVE_ID = "com.logica.hummingbird.hummingbirdmcscore.perspectives.McsHomePerspective";
 
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
