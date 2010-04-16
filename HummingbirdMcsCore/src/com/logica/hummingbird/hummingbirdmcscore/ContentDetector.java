5fedb7dbf86a83141e05121166a35fbc404f8653
Mark Doyle <markjohndoyle@googlemail.com>
2010-04-16 16:54:20 +0200

    Added foundations for welcome content.

---
+++ HummingbirdMcsCore/src/com/logica/hummingbird/hummingbirdmcscore/ContentDetector.java
@@ -0,0 +1,17 @@
+package com.logica.hummingbird.hummingbirdmcscore;
+import org.eclipse.ui.intro.IntroContentDetector;
+
+
+public class ContentDetector extends IntroContentDetector {
+
+	public ContentDetector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isNewContentAvailable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ HummingbirdMcsCore/src/com/logica/hummingbird/hummingbirdmcscore/perspectives/McsHomePerspective.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.logica.hummingbird.hummingbirdmcscore.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * @author doylemr
+ *
+ */
+public class McsHomePerspective implements IPerspectiveFactory {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
