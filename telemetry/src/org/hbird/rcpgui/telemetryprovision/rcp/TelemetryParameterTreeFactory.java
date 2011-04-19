a970769605aa362439d079071d412aa246929e8d
Mark Doyle <markjohndoyle@googlemail.com>
2011-04-19 21:55:00 +0200

    Fixed the parameter provider service selector.

---
+++ telemetry/src/org/hbird/rcpgui/telemetryprovision/rcp/TelemetryParameterTreeFactory.java
@@ -0,0 +1,18 @@
+package org.hbird.rcpgui.telemetryprovision.rcp;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+
+public class TelemetryParameterTreeFactory implements IAdapterFactory {
+
+	@Override
+	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		return null;
+	}
+
+	@Override
+	public Class[] getAdapterList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- telemetry/src/org/hbird/rcpgui/telemetryprovision/source/ParameterSource.java
+++ telemetry/src/org/hbird/rcpgui/telemetryprovision/source/ParameterSource.java
@@ -8,24 +8,38 @@ import java.util.Map;
 import org.hbird.rcpgui.parameterprovider.ParameterObserver;
 import org.hbird.rcpgui.parameterprovider.ParameterProvider;
 import org.hbird.rcpgui.parameterprovider.model.Parameter;
-import org.hbird.rcpgui.telemetryprovision.TelemetryActivator;
+import org.hbird.rcpgui.telemetryprovision.TelemetryProvisionActivator;
 import org.hbird.rcpgui.telemetryprovision.model.AbstractPropChangeModelObject;
 import org.hbird.rcpgui.telemetryprovision.model.TelemetryParameter;
 
 public class ParameterSource extends AbstractPropChangeModelObject implements ParameterObserver {
 
+	/** Current {@link ParameterProvider} service **/
+	private ParameterProvider parameterProvider;
+
+	/** List of all available {@linkplain ParameterProvider} services **/
+	private List<ParameterProvider> parameterProviderServices = new ArrayList<ParameterProvider>();
+
 
 	private final Map<String, TelemetryParameter> liveParameters = new HashMap<String, TelemetryParameter>();
 	private List<TelemetryParameter> liveParameterList = new ArrayList<TelemetryParameter>();
-
-	private final ParameterProvider parameterProvider;
 	private final boolean requestAll;
 	private List<String> interestList;
 	private boolean provisionActive = false;
 
+
 	public ParameterSource(boolean requestAll) {
+		final Object[] serviceObjects = TelemetryProvisionActivator.getParameterProviderServices().getServices();
+		if (serviceObjects.length > 0) {
+			for (final Object o : serviceObjects) {
+				parameterProviderServices.add((ParameterProvider) o);
+			}
+		}
+
 		this.requestAll = requestAll;
-		parameterProvider = (ParameterProvider) TelemetryActivator.getParameterProviderServices().getService();
+
+		// Use the first service by default
+		parameterProvider = parameterProviderServices.get(0);
 		parameterProvider.addObserver(this);
 	}
 
@@ -55,6 +69,7 @@ public class ParameterSource extends AbstractPropChangeModelObject implements Pa
 	}
 
 	public final String getProviderName() {
+		System.out.println("Getting provider name");
 		return parameterProvider.getProviderName();
 	}
 
@@ -90,6 +105,22 @@ public class ParameterSource extends AbstractPropChangeModelObject implements Pa
 		boolean oldVal = this.provisionActive;
 		this.provisionActive = provisionActive;
 		firePropertyChange("provisionActive", oldVal, provisionActive);
+	}
+
+	public List<ParameterProvider> getParameterProviderServices() {
+		return parameterProviderServices;
+	}
+
+	public ParameterProvider getParameterProvider() {
+		return parameterProvider;
+	}
+
+	public void setParameterProvider(ParameterProvider parameterProvider) {
+		this.parameterProvider = parameterProvider;
+	}
+
+	public void setParameterProviderServices(List<ParameterProvider> parameterProviderServices) {
+		this.parameterProviderServices = parameterProviderServices;
 	}
 
 }
