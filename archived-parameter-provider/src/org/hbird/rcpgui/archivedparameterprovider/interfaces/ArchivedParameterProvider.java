be73197e890bde9922dadb91051d3a348c6d82fa
Mark Doyle <markjohndoyle@googlemail.com>
2012-05-28 18:03:52 +0200

    OSGi archived parameter provider and provided implementation complete.

---
+++ archived-parameter-provider/src/org/hbird/rcpgui/archivedparameterprovider/interfaces/ArchivedParameterProvider.java
@@ -0,0 +1,15 @@
+package org.hbird.rcpgui.archivedparameterprovider.interfaces;
+
+import java.util.List;
+
+import org.hbird.core.commons.tmtc.Parameter;
+import org.joda.time.DateTime;
+
+public interface ArchivedParameterProvider {
+
+	List<Parameter> getParametersByQualifiedName(String QualifiedName, int numberOfParameters);
+
+	List<Parameter> getParametersByDescription(String description, int numberOfParameters);
+
+	List<Parameter> getParametersByReceivedTime(DateTime startTime, DateTime endTime, int numberOfParameters);
+}
